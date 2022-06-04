43fb22e890f3bc3964ecd9d115a10de2e6805c42
Jean Viegas <jeanmelg@gmail.com>
2022-06-03 22:25:25 -0300

    fix: alter convert string to bool method

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const { PORT } = process.env;
+const { PORT, UPSIDEDOWN_MODE } = process.env;
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -18,7 +18,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = JSON.parse(process.env.UPSIDEDOWN_MODE);
+const hereIsTheUpsideDown = UPSIDEDOWN_MODE === true;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
